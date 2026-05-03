34baed35605c44c19de032a01d95ddcbcb5774e0
Owner <tommy66721674@gmail.com>
2026-05-03 21:45:59 +0800

    feat(terraform): adding more variables

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,3 +1,4 @@
+# terraform/variables.tf
 variable "project_name" {
   description = "Name of the project (e.g., project-1, project-2)"
   type        = string
@@ -6,4 +7,40 @@ variable "project_name" {
 variable "environment" {
   description = "Deployment environment (dev, prod, etc.)"
   type        = string
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "azs" {
+  description = "List of availability zones"
+  type        = list(string)
+  default     = ["eu-west-1a", "eu-west-1b"]
+}
+
+variable "private_subnets" {
+  description = "CIDR blocks for private subnets"
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+variable "public_subnets" {
+  description = "CIDR blocks for public subnets"
+  type        = list(string)
+  default     = ["10.0.101.0/24", "10.0.102.0/24"]
+}
+
+variable "enable_nat_gateway" {
+  description = "Enable NAT gateway for private subnets"
+  type        = bool
+  default     = true
+}
+
+variable "single_nat_gateway" {
+  description = "Use a single NAT gateway (cheaper for non‑production)"
+  type        = bool
+  default     = true
 }
