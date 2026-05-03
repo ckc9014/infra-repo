8bc96d0c4a50cd0f022089e54abed8bf88438a8a
Owner <tommy66721674@gmail.com>
2026-05-03 21:20:23 +0800

    feat(terraform): added variables.tf

---
+++ terraform/variables.tf
@@ -0,0 +1,9 @@
+variable "project_name" {
+  description = "Name of the project (e.g., project-1, project-2)"
+  type        = string
+}
+
+variable "environment" {
+  description = "Deployment environment (dev, prod, etc.)"
+  type        = string
+}
