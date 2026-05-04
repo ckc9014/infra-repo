d2ec3f0ff1e6aa6afc965079870ca08beba527bc
ckctommy0914 <tommy66721674@gmail.com>
2026-05-04 16:56:47 +0800

    feat(terraform): add eks-cluster

---
+++ terraform/eks-cluster.tf
@@ -0,0 +1,27 @@
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 20.0"
+
+  cluster_name    = "${local.name_prefix}-cluster"
+  cluster_version = "1.30"
+
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
+
+  # Enable IAM Roles for Service Accounts (IRSA)
+  enable_irsa = true
+
+  
+  authentication_mode = "API_AND_CONFIG_MAP"
+
+  # Tags that Karpenter uses to discover resources.
+  cluster_tags = {
+    "karpenter.sh/discovery" = local.name_prefix
+  }
+
+  tags = {
+    Project     = var.project_name
+    Environment = var.environment
+    ManagedBy   = "Terraform"
+  }
+}
