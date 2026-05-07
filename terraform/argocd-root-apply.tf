04ee12e419791f9dfa4313784a97099437859c93
ckctommy0914 <tommy66721674@gmail.com>
2026-05-07 16:46:50 +0800

    feat(terraform): adding karpenter nodepool, ec2 nodeclass, and refactor argocd-apply to k8s-apply

--- terraform/argocd-root-apply.tf
+++
@@ -1,9 +0,0 @@
-resource "kubernetes_manifest" "apps_root" {
-  depends_on = [helm_release.argocd]
-  manifest   = yamldecode(file("${path.module}/../argocd/apps-root.yaml"))
-}
-
-resource "kubernetes_manifest" "platform_root" {
-  depends_on = [helm_release.argocd]
-  manifest   = yamldecode(file("${path.module}/../argocd/platform-root.yaml"))
-}
--- terraform/locals.tf
+++ terraform/locals.tf
@@ -1,3 +1,28 @@
 locals {
   name_prefix = "${var.project_name}-${var.environment}"
 }
+
+locals {
+  apps_root_path    = "${path.module}/../argocd/apps-root.yaml"
+  platform_root_path = "${path.module}/../argocd/platform-root.yaml"
+}
+
+
+locals {
+  nodepool_manifest = templatefile("${path.module}/../karpenter/nodepool.yaml.tmpl", {})
+
+  ec2nodeclass_manifest = templatefile("${path.module}/../karpenter/ec2nodeclass.yaml.tmpl", {
+    cluster_name       = module.eks.cluster_name
+    karpenter_node_role = aws_iam_role.karpenter_node.name
+  })
+}
+
+resource "kubernetes_manifest" "karpenter_nodepool" {
+  depends_on = [helm_release.karpenter]  
+  manifest   = yamldecode(local.nodepool_manifest)
+}
+
+resource "kubernetes_manifest" "karpenter_ec2nodeclass" {
+  depends_on = [helm_release.karpenter]
+  manifest   = yamldecode(local.ec2nodeclass_manifest)
+}
