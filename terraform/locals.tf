90e0a36397d5bde8e6497ee50d02b23d83a9ab38
ckctommy0914 <tommy66721674@gmail.com>
2026-05-07 17:54:35 +0800

    refactor(terraform): use local cluster name in locals.tf

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -1,20 +1,16 @@
 locals {
   name_prefix = "${var.project_name}-${var.environment}"
   cluster_name = "${local.name_prefix}-cluster"
-}
 
-locals {
-  apps_root_path    = "${path.module}/../argocd/apps-root.yaml"
+  apps_root_path     = "${path.module}/../argocd/apps-root.yaml"
   platform_root_path = "${path.module}/../argocd/platform-root.yaml"
 }
 
 
 locals {
   nodepool_manifest = templatefile("${path.module}/../karpenter/nodepool.yaml.tmpl", {})
-
   ec2nodeclass_manifest = templatefile("${path.module}/../karpenter/ec2nodeclass.yaml.tmpl", {
-    cluster_name       = module.eks.cluster_name
+    cluster_name       = local.cluster_name
     karpenter_node_role = aws_iam_role.karpenter_node.name
   })
 }
-
