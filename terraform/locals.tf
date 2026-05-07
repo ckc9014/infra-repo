43ee34db5281e4055ebdbe21cb9990c93b32eb2d
ckctommy0914 <tommy66721674@gmail.com>
2026-05-07 16:56:45 +0800

    refactor(terraform): removed duplicate code

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -17,12 +17,3 @@ locals {
   })
 }
 
-resource "kubernetes_manifest" "karpenter_nodepool" {
-  depends_on = [helm_release.karpenter]  
-  manifest   = yamldecode(local.nodepool_manifest)
-}
-
-resource "kubernetes_manifest" "karpenter_ec2nodeclass" {
-  depends_on = [helm_release.karpenter]
-  manifest   = yamldecode(local.ec2nodeclass_manifest)
-}
