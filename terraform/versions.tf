dce8010e4130077fd889a3413f9fcb5984491665
ckctommy0914 <tommy66721674@gmail.com>
2026-05-07 17:51:30 +0800

    refactor(terraform): use data to retrieve cluster name and auth

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -17,24 +17,26 @@ terraform {
   }
 }
 
+data "aws_eks_cluster" "cluster" {
+  name = local.cluster_name
+  depends_on = [module.eks]   
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = local.cluster_name
+  depends_on = [module.eks]
+}
+
 provider "kubernetes" {
-  host                   = module.eks.cluster_endpoint
-  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-  exec {
-    api_version = "client.authentication.k8s.io/v1beta1"
-    command     = "aws"
-    args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
-  }
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
 }
 
 provider "helm" {
   kubernetes = {
-    host                   = module.eks.cluster_endpoint
-    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-    exec = {
-      api_version = "client.authentication.k8s.io/v1beta1"
-      command     = "aws"
-      args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
-    }
+    host                   = data.aws_eks_cluster.cluster.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
+    token                  = data.aws_eks_cluster_auth.cluster.token
   }
 }
