6cc757c67820838af08e9b01b9121b1dcdcdaa2b
ckctommy0914 <tommy66721674@gmail.com>
2026-05-08 14:57:47 +0800

    refactor(terraform): remove k8s and helm provider

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -21,26 +21,3 @@ provider "aws"{
   region = var.aws_region
 }
 
-data "aws_eks_cluster" "cluster" {
-  name = local.cluster_name
-  depends_on = [module.eks]   
-}
-
-data "aws_eks_cluster_auth" "cluster" {
-  name = local.cluster_name
-  depends_on = [module.eks]
-}
-
-provider "kubernetes" {
-  host                   = data.aws_eks_cluster.cluster.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
-  token                  = data.aws_eks_cluster_auth.cluster.token
-}
-
-provider "helm" {
-  kubernetes = {
-    host                   = data.aws_eks_cluster.cluster.endpoint
-    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
-    token                  = data.aws_eks_cluster_auth.cluster.token
-  }
-}
