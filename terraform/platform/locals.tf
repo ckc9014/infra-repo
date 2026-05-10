c3837c0370ad32306b0693d8e3c0a2059d788a91
Owner <tommy66721674@gmail.com>
2026-05-10 12:34:40 +0800

    refactor(terraform): use data to retrieve data from infra

--- terraform/platform/locals.tf
+++ terraform/platform/locals.tf
@@ -1,7 +1,9 @@
 locals {
-  nodepool_manifest = templatefile("${path.module}/../karpenter/nodepool.yaml.tmpl", {})
-  ec2nodeclass_manifest = templatefile("${path.module}/../karpenter/ec2nodeclass.yaml.tmpl", {
-    cluster_name       = var.cluster_name
-    karpenter_node_role = var.karpenter_node_role_name
+  nodepool_manifest = templatefile("${path.module}/../../karpenter/nodepool.yaml.tmpl", {})
+
+
+  ec2nodeclass_manifest = templatefile("${path.module}/../../karpenter/ec2nodeclass.yaml.tmpl", {
+    cluster_name       = data.terraform_remote_state.infra.outputs.cluster_name
+    karpenter_node_role = data.terraform_remote_state.infra.outputs.karpenter_node_role_name
   })
 }
