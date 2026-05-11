1197b1bc846cbe4004f5d6639a074b82605aa19f
ckctommy0914 <tommy66721674@gmail.com>
2026-05-11 12:54:24 +0800

    feat(terraform): added public access for cluster

--- terraform/infrastructure/eks-cluster.tf
+++ terraform/infrastructure/eks-cluster.tf
@@ -5,8 +5,12 @@ module "eks" {
   cluster_name    = local.cluster_name
   cluster_version = "1.30"
 
+  
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
+
+  cluster_endpoint_public_access  = true  
+  
 
   # Enable IAM Roles for Service Accounts (IRSA)
   enable_irsa = true
