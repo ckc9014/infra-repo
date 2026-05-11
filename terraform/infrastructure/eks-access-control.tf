c2ab3bdcfe5a0dd5e36ab6daf7d30a59bc2319a1
ckctommy0914 <tommy66721674@gmail.com>
2026-05-11 17:06:21 +0800

    fix(terraform): the oidc need more privilege, set it to cluster admin

--- terraform/infrastructure/eks-access-control.tf
+++ terraform/infrastructure/eks-access-control.tf
@@ -5,9 +5,9 @@ resource "aws_eks_access_entry" "github_actions" {
   type              = "STANDARD"
 }
 
-resource "aws_eks_access_policy_association" "github_actions_view" {
+resource "aws_eks_access_policy_association" "github_actions_admin" {
   cluster_name  = module.eks.cluster_name
-  policy_arn    = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSAdminViewPolicy"
+  policy_arn    = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
   principal_arn = aws_eks_access_entry.github_actions.principal_arn
   access_scope {
     type = "cluster"
