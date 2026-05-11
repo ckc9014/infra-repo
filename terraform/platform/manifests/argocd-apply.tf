f3bb79baffa80a5dc28d4717719854ba02d57564
Owner <tommy66721674@gmail.com>
2026-05-12 00:08:24 +0800

    fix(terraform): remove depend on in the manifests as it is not necessary

--- terraform/platform/manifests/argocd-apply.tf
+++ terraform/platform/manifests/argocd-apply.tf
@@ -1,9 +1,7 @@
 resource "kubernetes_manifest" "apps_root" {
-  depends_on = [helm_release.argocd]
   manifest   = yamldecode(file(local.apps_root_path))
 }
 
 resource "kubernetes_manifest" "platform_root" {
-  depends_on = [helm_release.argocd]
   manifest   = yamldecode(file(local.platform_root_path))
 }
--- terraform/platform/manifests/karpenter-crds.tf
+++ terraform/platform/manifests/karpenter-crds.tf
@@ -1,9 +1,7 @@
 resource "kubernetes_manifest" "karpenter_nodepool" {
-  depends_on = [helm_release.karpenter]
   manifest   = yamldecode(local.nodepool_manifest)
 }
 
 resource "kubernetes_manifest" "karpenter_ec2nodeclass" {
-  depends_on = [helm_release.karpenter]
   manifest   = yamldecode(local.ec2nodeclass_manifest)
 }
