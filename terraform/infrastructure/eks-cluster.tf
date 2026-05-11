3e50b17574566c821c736883e58ab42638724f36
Owner <tommy66721674@gmail.com>
2026-05-11 20:47:57 +0800

    fix(terraform): change from t3.micro to t3.small

--- terraform/infrastructure/eks-cluster.tf
+++ terraform/infrastructure/eks-cluster.tf
@@ -32,7 +32,7 @@ module "eks" {
   eks_managed_node_groups = {
     system = {
       name = "system-node-group"
-      instance_types = ["t3.micro"]
+      instance_types = ["t3.small"]
       min_size     = 1
       max_size     = 3
       desired_size = 1
