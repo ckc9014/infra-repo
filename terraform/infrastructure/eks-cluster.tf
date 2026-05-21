14ef31d38d132a68e90ae0ae890ca7bb9e8cc084
ckctommy0914 <tommy66721674@gmail.com>
2026-05-21 09:47:19 +0800

    fix(terraform): reverted the coredns to default setting

--- terraform/infrastructure/eks-cluster.tf
+++ terraform/infrastructure/eks-cluster.tf
@@ -22,27 +22,8 @@ module "eks" {
 
     coredns = {
       most_recent = true
-      # Override the CoreDNS configuration to forward to Google's public DNS
-      configuration_values = jsonencode({
-        corefile = <<-EOT
-          .:53 {
-              errors
-              health
-              ready
-              kubernetes cluster.local in-addr.arpa ip6.arpa {
-                  pods insecure
-                  fallthrough in-addr.arpa ip6.arpa
-              }
-              prometheus :9153
-              forward . 8.8.8.8 8.8.4.4
-              cache 30
-              loop
-              reload
-              loadbalance
-          }
-        EOT
-      })
-    }
+      }
+    
     kube-proxy = {
       most_recent = true
     }
