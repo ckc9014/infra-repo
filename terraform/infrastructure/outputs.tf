489e480321dff0414a736e2d3394810bf59de528
ckctommy0914 <tommy66721674@gmail.com>
2026-05-21 12:16:16 +0800

    feat(workflows): added output job and output for the training bucket

--- terraform/infrastructure/outputs.tf
+++ terraform/infrastructure/outputs.tf
@@ -30,3 +30,8 @@ output "cluster_certificate_authority_data" {
 output "karpenter_node_role_name" {
   value = aws_iam_role.karpenter_node.name
 }
+
+output "model_bucket_name" {
+  description = "Name of the S3 bucket for training outputs"
+  value       = aws_s3_bucket.training.id
+}
