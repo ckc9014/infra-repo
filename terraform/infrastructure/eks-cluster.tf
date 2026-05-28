426190b9dd1627ab7b6986ae35943455a3142b7e
ckctommy0914 <tommy66721674@gmail.com>
2026-05-28 17:01:04 +0800

    feat(infra): added aws balancer controller as add-on in EKS

--- terraform/infrastructure/eks-cluster.tf
+++ terraform/infrastructure/eks-cluster.tf
@@ -30,6 +30,10 @@ module "eks" {
     vpc-cni = {
       most_recent = true
     }
+    aws-load-balancer-controller = {
+      addon_version = "v3.3.0"               
+      service_account_role_arn = aws_iam_role.aws_lb_controller.arn 
+  }
   }
 
   # Tags that Karpenter uses to discover resources.
